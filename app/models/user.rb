9afed8e33bd35cf9a2ac9ef493526d7dffab3830
Melissa Holmes <melissa@bignerdranch.com>
2013-11-17 22:29:06 -0500

    Add validations for User and relevant specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true
+	validates :email, presence: true,
+                    uniqueness: { case_sensitive: false }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	subject(:user) { described_class.new(name: "First Last", email: "test@example.com") }
+
+	describe 'validations' do
+		it 'should be valid with a name and email' do
+			expect(user).to be_valid
+		end
+
+		it 'should not be valid without a name' do
+			user.name = " "
+			expect(user).to_not be_valid
+		end
+
+		it 'should not be valid without an email' do
+			user.email = " "
+			expect(user).to_not be_valid
+		end
+	end
 end
