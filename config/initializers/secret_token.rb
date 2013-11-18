c4c08840e2e2f24d4fa6907f70968cf5fbbb283f
Melissa Holmes <melissa@bignerdranch.com>
2013-11-17 20:45:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameNight::Application.config.secret_key_base = 'd85a69501ba28ba9c2e5a85ab7c6402f38a132efa9c715c1c7a5f9f4b276b2edc456f466f2bdb7f78eb1fcb9b7512a5ed0ff44aa845bff6a8b5aa8ee5502fbe4'
