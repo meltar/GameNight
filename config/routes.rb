6c12f09df87e2294880486f656e9c8fcfc3a3a8d
Melissa Holmes <melissa@bignerdranch.com>
2013-11-17 21:42:06 -0500

    Generate UsersController and add routes to config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 GameNight::Application.routes.draw do
+	root to: redirect('/signin')
+	resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
