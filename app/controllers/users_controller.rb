94e1771d265fe1378fa1bbadd9db344d5c237407
Melissa Holmes <melissa@bignerdranch.com>
2013-11-17 22:08:10 -0500

    Add basic behavior to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+
+		redirect_to users_path
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email)
+	end
+
 end
